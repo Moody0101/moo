b31f041eea6fe5005e1279621603681565b28bc1
Moody0101 <azmoudh@gmail.com>
2021-09-22 14:47:39 +0100

    secured the db and also I added some featuures :) you can see the source code for more demonstration

--- gitcommands/gitToken.py
+++ gitcommands/gitToken.py
@@ -1,7 +1,9 @@
 from json import dumps, load
-from os import path, getcwd
+from os import path, system, mkdir
 from sys import argv
 from collections import deque
+
+
 usage: str = """
 
 gitToken [--store] [--get] [--token]
@@ -22,13 +24,38 @@ if you want to store a token then pass these args:
 class Token_:
 
     def __init__(self):
-        self.JsonPath = 'data.json'
+        self.the_set = dumps({'token': [], 'exp': []})
+        self.dir = "c:/users/pc/documents/data"
+        self.JsonPath = "c:/users/pc/documents/data/data.json"
+        self.check_()
+    def check_(self):
+        if not path.exists(self.dir):
+            mkdir(self.dir)
+        if not path.isfile(self.JsonPath):
+
+            open(self.JsonPath, 'x').write(self.the_set)
+            print("made file")
+        else:
+            pass
+        
+        system(f"attrib {self.JsonPath} +H")
+    def secureDB(self):
+        if not path.exists(self.JsonPath):
+            print("db does not exist")
+            exit()
+        system(f"attrib {self.JsonPath} +H")
+    def clearDB(self):
+        with open(self.JsonPath, 'w+') as f:
+            f.write(self.the_set)
+            print('cleared successfully!!')
+    def openDb(self):
+        system(f"attrib {self.JsonPath} -H")
     def storeData(self):
         if path.exists(self.JsonPath):
-            data0 = load(open(self.JsonPath, 'r'))['token']
+            data0 = load(open(self.JsonPath, 'r+'))['token']
             ddata0 = deque(data0)
             ddata0.appendleft(argv[2])
-            data1 = load(open(self.JsonPath, 'r'))['exp']
+            data1 = load(open(self.JsonPath, 'r+'))['exp']
             ddata1 = deque(data1)
             ddata1.appendleft(argv[3])
             all_data = dumps({
@@ -37,8 +64,8 @@ class Token_:
                     })
             open(self.JsonPath, 'w+').write(all_data)
         else:
-            the_set = dumps({'token': [], 'exp': []})
-            open(self.JsonPath, 'w+').write(the_set)
+            
+            open(self.JsonPath, 'w+').write(self.the_set)
             self.storeData()
     def getData(self):
         if path.exists(self.JsonPath):
@@ -62,6 +89,8 @@ class Token_:
                     print(usage)
                 else:
                     self.storeData()
+            elif argv[1] == "--clear":
+                self.clearDB()
             elif argv[1] == "--version":
                 print('a part of the moo package v: 0.0.1')
             else:
@@ -72,10 +101,9 @@ class Token_:
 def main():
 
     tkn = Token_()
+    tkn.openDb()
     tkn.execute()
-
-
+    tkn.secureDB()
 
 if __name__ == '__main__':
     main()
-
